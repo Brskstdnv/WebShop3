07c2360471ab0839611d6854e0c364e547d039ba
Brskstdnv <bobara188@gmail.com>
2025-01-21 11:04:23 +0200

    webshop5

--- WebShopApp/WebShopApp.Core/obj/Debug/net6.0/WebShopApp.Core.AssemblyInfo.cs
+++ WebShopApp/WebShopApp.Core/obj/Debug/net6.0/WebShopApp.Core.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebShopApp.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b527570c9a87d8877097980c88f04946a7847b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebShopApp.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebShopApp.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
